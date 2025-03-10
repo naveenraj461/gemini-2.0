27613f921b9cc73f20584622f0bcec79fb5c7447
naveenraj461 <naveenraj461@gmail.com>
2025-03-10 18:06:21 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 
 import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
 
 
